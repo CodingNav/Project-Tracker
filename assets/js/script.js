4a8911e0aaf24b4bdfae981a2daa73bf471e49cf
CodingNav <navleenkaurr@gmail.com>
2021-07-08 19:42:44 -0400

    Added bootstrap modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ setInterval(function () {
     var currentTime = moment().format("MMM Do, YYYY [at] hh:m:ss a");
     $("#current-time").text(currentTime);
 }, 1000);
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#add-project').trigger('focus')
+  })
