b7ce09c8ec1dfbd14254366dc999c7cdd214d9e2
CodingNav <navleenkaurr@gmail.com>
2021-07-09 18:07:31 -0400

    Added variables and eventListener to submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+var projectName = document.querySelector("#project-name");
+var projectType = document.querySelector("#project-type");
+var hourlyRate = document.querySelector(".hourly-rate");
+var date = document.querySelector("#date-picker");
+var submitButton = document.querySelector("#submit-button");
 
 setInterval(function () {
     var currentTime = moment().format("MMM Do, YYYY [at] hh:mm:ss a");
@@ -11,3 +16,14 @@ $('#myModal').on('shown.bs.modal', function () {
 $(function () {
     $("#datepicker").datepicker({ minDate: 0,});
 });
+
+
+submitButton.addEventListener('click', function() {
+    var projectData = {
+        name: projectName.value,
+        type: projectType.value,
+        rate: hourlyRate.value,
+        date: date.value
+    }
+
+})
